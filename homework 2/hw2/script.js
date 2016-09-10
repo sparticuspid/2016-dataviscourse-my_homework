ca8ad84b7e1895f4fad9a6626348281b0f69956b
Clark Barnett <u1119424@utah.edu>
2016-09-10 03:10:53 -0600

    Part III: Reshaping Things with D3

--- homework 2/hw2/script.js
+++ homework 2/hw2/script.js
@@ -2,7 +2,17 @@
 // These keep JSHint quiet if you're using it (highly recommended!)
 
 function staircase() {
-    alert("you pressed the staircase button, didn't you? oh you're such a good boy, aren't you?");
+    var c = document.getElementById("barChart1").children;
+    var i;
+    for (i = 0; i < c.length; i++) {
+        var height = (i+1)*10;
+        c[i].setAttribute("width", 10);
+        c[i].setAttribute("height", height);
+        c[i].setAttribute("y", 190-height);
+        c[i].setAttribute("x", i*10);
+    }
+    //alert("children: " + c);
+    //barChart1 = document.getElementById("barChart1");
 }
 
 function update(error, data) {
@@ -49,7 +59,31 @@ function update(error, data) {
 
     // TODO: Select and update the 'a' bar chart bars
 
+    var barChart1 = d3.select("#barChart1")
+    var selection = barChart1.selectAll("rect")
+        .data(data)
+        .attr("x", 10)
+        .attr("y", function (d,i) { 
+            return iScale(i);
+        })
+        .attr("width", function (d) {
+                return aScale(d.a);
+        })
+        .attr("height", 10)
+
     // TODO: Select and update the 'b' bar chart bars
+
+    var barChart2 = d3.select("#barChart2")
+    var selection = barChart2.selectAll("rect")
+        .data(data)
+        .attr("x", 10)
+        .attr("y", function (d,i) { 
+            return iScale(i);
+        })
+        .attr("width", function (d) {
+            return bScale(d.b);
+        })
+        .attr("height", 10)
 
     // TODO: Select and update the 'a' line chart path using this line generator
     var aLineGenerator = d3.line()
@@ -60,7 +94,24 @@ function update(error, data) {
             return aScale(d.a);
         });
 
+    var lineChart1 = d3.select("#lineChart1")
+    var selection = lineChart1.selectAll("path")
+        .attr("d", aLineGenerator(data));
+
+
     // TODO: Select and update the 'b' line chart path (create your own generator)
+    var bLineGenerator = d3.line()
+        .x(function (d, i) {
+            return iScale(i);
+        })
+        .y(function (d) {
+            return aScale(d.b);
+        });
+
+    var lineChart2 = d3.select("#lineChart2")
+    var selection = lineChart2.selectAll("path")
+        .attr("d", bLineGenerator(data));
+
 
     // TODO: Select and update the 'a' area chart path using this line generator
     var aAreaGenerator = d3.area()
@@ -72,9 +123,38 @@ function update(error, data) {
             return aScale(d.a);
         });
 
+    var areaChart1 = d3.select("#areaChart1")
+    var selection = areaChart1.selectAll("path")
+        .attr("d", aAreaGenerator(data));
+
     // TODO: Select and update the 'b' area chart path (create your own generator)
+    var bAreaGenerator = d3.area()
+        .x(function (d, i) {
+            return iScale(i);
+        })
+        .y0(0)
+        .y1(function (d) {
+            return bScale(d.b);
+        });
+
+    var areaChart2 = d3.select("#areaChart2")
+    var selection = areaChart2.selectAll("path")
+        .attr("d", bAreaGenerator(data));
 
     // TODO: Select and update the scatterplot points
+
+    var scatterPlot = d3.select("#scatterPlot")
+    var selection = scatterPlot.selectAll("circle")
+        .data(data)
+        .attr("cx", function (d) {
+            return aScale(d.a);
+        })
+        .attr("cy", function (d) { 
+            return bScale(d.b);
+        })
+        .on('click', function(d,i){ 
+            console.log('hi')
+        });
 
     // ****** TODO: PART IV ******
 }
@@ -83,7 +163,6 @@ function changeData() {
     // // Load the file indicated by the select menu
     var dataFile = document.getElementById('dataset').value;
     if (document.getElementById('random').checked) {
-        alert("did you select a new field?");
         randomSubset();
     }
     else{
@@ -95,7 +174,6 @@ function randomSubset() {
     // Load the file indicated by the select menu,
     // and then slice out a random chunk before
     // passing the data to update()
-    alert("randomSubset")
     var dataFile = document.getElementById('dataset').value;
     if (document.getElementById('random').checked) {
         d3.csv('data/' + dataFile + '.csv', function (error, data) {
