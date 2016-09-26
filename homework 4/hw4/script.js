8593d27701a86e5dc4e0234a8adc7e5fe16124c9
Clark Barnett <u1119424@utah.edu>
2016-09-26 01:44:50 -0600

    Complete Partial Homework 4: Part II & III

--- homework 4/hw4/script.js
+++ homework 4/hw4/script.js
@@ -17,7 +17,7 @@ var goalsMadeHeader = 'Goals Made',
 
 /** Setup the scales*/
 var goalScale = d3.scaleLinear()
-    .range([cellBuffer, 2 * cellWidth - cellBuffer]);
+    .range([cellBuffer, 2 * cellWidth - cellBuffer])
 
 /**Used for games/wins/losses*/
 var gameScale = d3.scaleLinear()
@@ -45,8 +45,6 @@ var rank = {
     'Group': 0
 };
 
-
-
 //For the HACKER version, comment out this call to d3.json and implement the commented out
 // d3.csv call below.
 
@@ -67,6 +65,7 @@ var rank = {
 d3.csv("data/fifa-matches.csv", function (error, csvData) {
 
    // ******* TODO: PART I *******
+   console.log(csvData)
     teamData = d3.nest()
     .key(function (d) {
         return d.Team;
@@ -75,30 +74,30 @@ d3.csv("data/fifa-matches.csv", function (error, csvData) {
         return {
             "Wins": d3.sum(leaves,function(d){return d.Wins}),
             "Losses": d3.sum(leaves,function(d){return d.Losses}),
-            "Goals Made": d3.sum(leaves,function(d){return d["Goals Made"]}),
-            "Goals Conceded": d3.sum(leaves,function(d){return d["Goals Conceded"]}),
+            "Goals Made": d3.sum(leaves,function(d){return d[goalsMadeHeader]}),
+            "Goals Conceded": d3.sum(leaves,function(d){return d[goalsConcededHeader]}),
             "Delta Goals": d3.sum(leaves,function(d){return d["Delta Goals"]}),
             "TotalGames": leaves.length,
             "Result":  {
                 "label": d3.max(leaves,function(d){return d.Result}),
-                "ranking": leaves.length - 2
+                "ranking": rank[d3.max(leaves,function(d){return d.Result})]
             }, 
             "type": "aggregate",
             "games": 
                 d3.nest()
-                    .key(function (d) {
-                        return d.Opponent;
+                    .key(function (games) {
+                        return games.Opponent;
                     })
                     .rollup(function(games) {
                         return {
                             "Wins": [], //d3.sum(games,function(d){return d.Wins}),
                             "Losses": [], //d3.sum(games,function(d){return d.Losses}),
-                            "Goals Made": d3.sum(games,function(d){return d["Goals Made"]}),
-                            "Goals Conceded": d3.sum(games,function(d){return d["Goals Conceded"]}),
+                            "Goals Made": d3.sum(games,function(d){return d[goalsMadeHeader]}),
+                            "Goals Conceded": d3.sum(games,function(d){return d[goalsConcededHeader]}),
                             "Delta Goals": [], //d3.sum(games,function(d){return d["Delta Goals"]}),
                             "Result":  {
-                                "label": d3.max(leaves,function(d){return d.Result}),
-                                "ranking": leaves.length - 2
+                                "label": d3.max(games,function(d){return d.Result}),
+                                "ranking": rank[d3.max(games,function(d){return d.Result})]
                             }, 
                             "type": "game",
                             "Opponent": d3.max(games,function(d){return d.Team}),
@@ -109,6 +108,7 @@ d3.csv("data/fifa-matches.csv", function (error, csvData) {
     })
     .entries(csvData);
 
+    console.log(teamData)
     createTable();
     updateTable();
 });
@@ -136,9 +136,44 @@ d3.csv("data/fifa-tree.csv", function (error, csvData) {
  */
 function createTable() {
 
-// ******* TODO: PART II *******
+    // ******* TODO: PART II *******
 
-// ******* TODO: PART V *******
+    // ********* Add x-axis to Goals Column header **************
+
+    var tickCount = d3.max(teamData, function (d, i) {    
+                return d.value["Goals Made"]})/2;
+
+    goalScale
+        .domain([0, d3.max(teamData, function (d, i) {    
+            return d.value["Goals Made"];
+        })])
+
+    var goalAxis = d3.select("#goalHeader")
+        .append("svg")
+        .attr("id", "goal-Axis")
+        .attr("width", cellWidth * 2)
+        .attr("height", cellHeight + cellBuffer)
+
+    var goalAxisGroup = d3.select("#goalHeader").select('#goal-Axis')
+        .append("g")
+        .attr("transform", "translate(0,20)")
+
+
+    // create a new axis that has the ticks and labels on the top
+    var goalAxis = d3.axisTop()
+    // assign the scale to the axis
+        .scale(goalScale)
+        .ticks(tickCount)
+
+    goalAxisGroup
+        .call(goalAxis)
+
+
+    // ********* Create list to populate table **************
+
+    tableElements = teamData;   
+
+    // ******* TODO: PART V *******
 
 }
 
@@ -148,8 +183,194 @@ function createTable() {
  */
 function updateTable() {
 
-// ******* TODO: PART III *******
+    // ******* TODO: PART III *******
 
+    goalScale
+        .domain([0, d3.max(teamData, function (d, i) {    
+            return d.value["Goals Made"];
+        })])
+
+    var tr = d3.select("#matchTable").select("tbody").selectAll("tr")
+        .data(tableElements)
+        .enter()
+        .append("tr")
+
+    var td = tr.selectAll("td")
+        .data( function (d) {
+        return [{"type": d.value["type"], "vis": "team", "value": d.key},
+                {"type": d.value["type"], "vis": "goals", "value": {
+                    "Goals Made": d.value["Goals Made"],
+                    "Goals Conceded": d.value["Goals Conceded"],
+                    "Delta Goals": d.value["Delta Goals"]}
+                },
+                {"type": d.value["type"], "vis": "result", "value": d.value.Result["label"]},
+                {"type": d.value["type"], "vis": "bars", "value": d.value["Wins"]},
+                {"type": d.value["type"], "vis": "bars", "value": d.value["Losses"]},
+                {"type": d.value["type"], "vis": "bars", "value": d.value["TotalGames"]}]
+
+        })  
+
+    td
+        .enter()
+        .append("td")
+
+    gameScale
+        .domain([0, d3.max(teamData, function (d, i) {    
+            return d.value["TotalGames"];
+        })])
+
+    aggregateColorScale
+        .domain([0, d3.max(teamData, function (d, i) {    
+            return d.value["TotalGames"];
+        })])
+
+    td_bars = d3.select("#matchTable").select("tbody").selectAll("td")
+        .filter(function (d) {
+            return d.vis == 'bars'
+        })
+        .append("svg")
+            .attr("width", cellWidth + cellBuffer)
+            .attr("height", cellHeight)
+            .append("g")
+
+    td_bars_rect = td_bars
+        .append("rect")
+        .attr("width", function (d) {
+            return gameScale(d.value)
+        })
+        .attr("height", barHeight)
+        .attr('fill', function (d) {
+            return aggregateColorScale(d.value);
+        })
+
+    td_bars_text = td_bars
+        .append("text")
+        .attr("width", function (d) {
+            return gameScale(d.value)
+        })
+        .attr("x", function (d) {
+            return gameScale(d.value) - 10
+        })
+        .attr("y", cellBuffer)
+        .attr("fill", "white")
+        .text(function (d) {
+            return d.value
+        })
+
+    td_team = d3.select("#matchTable").select("tbody").selectAll("td")
+        .filter(function (d) {
+            return d.vis == 'team'
+        })
+        .attr("width", cellWidth*5)
+        .classed("teamName", true)
+        .text(function (d) {
+            return d.value
+        })
+
+    td_result = d3.select("#matchTable").select("tbody").selectAll("td")
+        .filter(function (d) {
+            return d.vis == 'result'
+        })
+        .append("svg")
+            .attr("width", cellWidth*2)
+            .attr("height", cellHeight)
+            .append("text")
+            .text(function (d) {
+                return d.value
+            })
+            .attr("y", cellBuffer)
+
+    td_goals = d3.select("#matchTable").select("tbody").selectAll("td")
+        .filter(function (d) {
+            return d.vis == 'goals'
+        })
+        .append("svg")
+            .attr("width", cellWidth*2)
+            .attr("height", cellHeight)
+
+    td_goals_made = td_goals
+        .append("circle")
+        .classed("goalCircle", true)
+        .classed("goalsMade", true)
+        .attr("cx", function (d) {
+            console.log(goalScale(d.value["Goals Made"]))
+            return goalScale(d.value["Goals Made"])
+        })
+        .attr("cy", cellBuffer)
+
+    td_goals_conceded = td_goals
+        .append("circle")
+        .classed("goalCircle", true)
+        .classed("goalsConceded", true)
+        .attr("cx", function (d) {
+            console.log(goalScale(d.value["Goals Conceded"]))
+            return goalScale(d.value["Goals Conceded"])
+        })
+        .attr("cy", cellBuffer)
+
+    td_delta_goals = td_goals
+        .append("rect")
+        .classed("goalBar", true)
+        .attr("x", function (d) {
+            console.log(goalScale(d.value["Goals Conceded"]))
+            return goalScale(d.value["Goals Conceded"])
+        })
+        .attr("height", 10)
+        .attr("width", function (d) {
+            console.log(goalScale(d.value["Goals Conceded"]))
+            return goalScale(d.value["Delta Goals"])
+        })
+        .attr("y", 10)
+
+        // .text(function (d) {
+        //     return d.value
+        // })               
+ 
+        // .filter(function (d) {
+        //     return d.vis == 'text'
+        // })
+        // .text(function (d) {
+        //     return d.value
+        // })
+
+    // td_bars = td.selectAll("svg")
+    //     .data( function (d) {
+    //         console.log(d)
+    //         return d
+    //     })
+    //     .enter()
+    //     .append("svg")
+    //     .filter(function (d) {
+    //         return d.vis == 'bars'
+    //     })   
+        
+
+    // td_bars = d3.select("#matchTable").select("tbody").selectAll("tr").selectAll("td").selectAll("svg")
+    //     .enter()
+    //     .filter(function (d) {
+    //         return d.vis == 'goals'
+    //     })
+
+    // td_bars
+    //     .enter()
+    //     .append("svg")
+
+
+    // td = td.filter(function (d) {
+    //     console.log(d.vis)
+    //     return d.vis == "text"
+    // })
+    //     .text(function (d) {
+    //         return d.value
+    //     })
+
+    // console.log(td)
+    // td
+
+    //     .text(function (d) {
+    //         console.log(d)
+    //         return d.type
+    //     })
 };
 
 
